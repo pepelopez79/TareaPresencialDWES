8d647a248bbd51027a5728c324ea109d0da43356
PepeLopez7 <114738363+pepelopez79@users.noreply.github.com>
2023-11-09 20:28:14 +0100

    ejercicio 2.2

--- includes/validar.php
+++ includes/validar.php
@@ -45,63 +45,63 @@ function valoresfrm() {
 }
 
 if (isset($_POST["submit"])) {
-  // Validación del número de expediente
+  // Número de expediente
   if (!empty($_POST["expediente"]) && preg_match("/^[A-Za-z]{2,5}-\d{4}-\d{4}\/[HM]$/", $_POST["expediente"])) {
     $expediente = trim($_POST["expediente"]);
   } else {
     $errors["expediente"] = "Formato de N.º de expediente incorrecto / El año debe ser anterior o igual al actual";
   }
 
-  // Validación del NIF
+  // NIF
   if (!empty($_POST["nif"]) && preg_match("/^\d{8}[A-Za-z]$/", $_POST["nif"])) {
     $nif = trim($_POST["nif"]);
   } else {
     $errors["nif"] = "El valor introducido no cumple el formato de lo NIF";
   }
 
-  // Validación de apellidos
+  // Apellidos
   if (!empty($_POST["apellidos"]) && preg_match("/^[A-Za-z_\- ]{1,20}$/", $_POST["apellidos"])) {
     $apellidos = trim($_POST["apellidos"]);
   } else {
     $errors["apellidos"] = "No puede tener más de 20 caracteres / No puede contener números";
   }
 
-  // Validación de nombre
+  // Nombre
   if (!empty($_POST["nombre"]) && preg_match("/^[A-Za-z_\- ]{1,10}$/", $_POST["nombre"])) {
     $nombre = trim($_POST["nombre"]);
   } else {
     $errors["nombre"] = "No puede tener mas de 10 caracteres / No puede contener números";
   }
 
-  // Validación de sexo
+  // Sexo
   if (!empty($_POST["sexo"]) && ($_POST["sexo"] == "H" || $_POST["sexo"] == "M")) {
     $sexo = $_POST["sexo"];
   } else {
     $errors["sexo"] = "Seleccione un sexo";
   }
 
-  // Validación de email
+  // Email
   if (!empty($_POST["email"]) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     $email = $_POST["email"];
   } else {
     $errors["email"] = "El valor introducido no cumple el formato de los emails";
   }
 
-  // Validación de teléfono móvil
+  // Teléfono móvil
   if (!empty($_POST["telefono"]) && preg_match("/^\d{9}$/", $_POST["telefono"])) {
     $telefono = trim($_POST["telefono"]);
   } else {
     $errors["telefono"] = "El valor introducido no cumple el formato de los teléfonos móviles";
   }
 
-  // Validación de imagen 
+  // Imagen 
   if (isset($_FILES["imagen"]) && !empty($_FILES["imagen"]["tmp_name"])) {
     $imagen = $_FILES["imagen"]["name"];
   } else {
     $errors["imagen"] = "Seleccione una imagen válida";
   }
 
-  // Validación de beca
+  // Beca
   $beca = isset($_POST["beca"]) ? "on" : "off";
 }
 
