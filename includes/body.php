a23f3e367883a90475be6ada1bf69ceb448394be
PepeLopez7 <114738363+pepelopez79@users.noreply.github.com>
2023-11-09 19:05:54 +0100

    ejercicio 1 finalizado

---
+++ includes/body.php
@@ -0,0 +1,3 @@
+<div class="encabezado text-center">	
+  <h1>2ºDAW Semi - López Salado, José </h1>
+</div>  
---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<div class="pie"> 
+  <div class="text-center" >
+      <p>DWES - TareaPresencial01</p>
+  </div>     
+</div>
