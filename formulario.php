f18eaaa2917fe1c40dcbfb625f8a7507f3d22309
PepeLopez7 <114738363+pepelopez79@users.noreply.github.com>
2023-11-09 18:56:28 +0100

    commit inicial

---
+++ formulario.php
@@ -0,0 +1,10 @@
+<html>
+  <head lang="es">
+    <?php require 'includes/header.php'; ?>
+  </head>
+  <body>
+    <?php require 'includes/valformfinal.php'; ?>
+    <?php require 'includes/body.php'; ?>
+    <?php require 'includes/footer.php'; ?>
+  </body>
+</html>
